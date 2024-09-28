d1a0e46adb2c1aa9e22e8dfd7a82223cbcd96381
Songdaewon <145010135+Songdaewon@users.noreply.github.com>
2024-09-28 18:36:32 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+package a;
+
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int gu = scan.nextInt();
+		
+		for(int i=1;i<=9;i++) {
+			System.out.println(gu+" * "+i+" = "+gu*i);
+		}
+		
+}
+}
+
+/*
+
+
+
+
+
+*/
