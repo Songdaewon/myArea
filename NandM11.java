f826896ff9ceedaeffd3910cb1ce58d035774d69
Songdaewon <145010135+Songdaewon@users.noreply.github.com>
2024-12-23 19:33:21 +0900

    Add files via upload

---
+++ NandM11.java
@@ -0,0 +1,60 @@
+package part5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class NandM11 {
+	static int n;
+	static int m;
+	static int arr[];
+	static int num[];
+
+	static Set<String> set=new LinkedHashSet<>();
+	static String str[];
+	static StringBuilder sb= new StringBuilder();
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		n=Integer.parseInt(st.nextToken());
+		m=Integer.parseInt(st.nextToken());
+
+		st = new StringTokenizer(br.readLine());
+		num=new int[n];
+		for(int i=0;i<n;i++) {
+			num[i]=Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(num);
+
+		arr=new int[m];
+
+		dfs(0);
+		for(String i:set) {
+			System.out.println(i);
+		}
+	}
+	static void dfs(int depth) {
+		if(depth==m) {
+			for(int i:arr) {
+				sb.append(i+" ");
+			}
+			set.add(sb.toString());
+			sb.setLength(0);
+
+			return;
+		}
+
+		for(int i=0;i<n;i++) {
+			arr[depth]=num[i];
+			dfs(depth+1);
+
+		}
+	}
+
+}
