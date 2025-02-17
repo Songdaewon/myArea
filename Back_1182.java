58e035508044e85a7633744534f38ad43d50b482
Songdaewon <145010135+Songdaewon@users.noreply.github.com>
2025-02-17 20:53:03 +0900

    Add files via upload

--- Back_1182.java
+++ Back_1182.java
@@ -1,36 +1,49 @@
 package part7;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Back_1182 {
-    static int[] num;
-    private static int N; // 정수의 개수
-    private static int S; // 정수의 합
-    private static int answer = 0;
+	static int[] num;
+	static int n; // 정수의 개수
+	static int s; // 정수의 합
+	static int answer = 0;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        S = sc.nextInt();
-        num = new int[N];
-        for (int i = 0; i < N; i++) {
-            num[i] = sc.nextInt();
-        }
-        dfs(0, 0);
-        if (S == 0) System.out.println(answer - 1); 
-        //S 0일때가 전체 합이 0 일때랑 겹치기 때문에 -1 을 해주어야 합니다.
-        //위의 사진에 공집합이 0이라고 볼 수 있습니다
-         else  System.out.println(answer);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-    }
+		n=Integer.parseInt(st.nextToken());
+		s=Integer.parseInt(st.nextToken());
+		num=new int[n];
+		
+		st=new StringTokenizer(br.readLine());
+		for(int i=0;i<n;i++) {
+			num[i]=Integer.parseInt(st.nextToken());
+		}
 
-    private static void dfs(int depth, int sum) {
-        if (depth == N) {
-            if (sum == S) answer++;
-            return;
-        }
-        //tree를 생각해서 직접 그려보면 이해하기가 쉽습니다.
-        dfs(depth + 1, sum + num[depth]);
-        dfs(depth + 1, sum); 
-    }
+		dfs(0,0);
+		//s==0이면, dfs가 0에서 시작하기에 아무것도 안더해도 0이 나와 answer가 무조건 1이됨
+		if(s==0)
+			System.out.println(answer-1);
+		else
+			System.out.println(answer);
+	}
+
+	private static void dfs(int depth, int sum) {
+		if(depth==n) {
+			if(sum==s)
+				answer++;
+			return;
+		}
+
+		dfs(depth+1,sum+num[depth]);
+		dfs(depth+1,sum);
+	}
 }
+
+
+
+
